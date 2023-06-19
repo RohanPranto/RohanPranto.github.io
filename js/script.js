cb5619f4742bd88f451a1f29f22750ca8555a1f8
Rohan Biswas <72951224+RohanPranto@users.noreply.github.com>
2023-06-19 12:50:29 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 const themeToggle = document.getElementById('theme-toggle');
-const themeIcon = document.querySelector('#theme-toggle .toggle-icon');
-const moonIcon = document.querySelector('#theme-toggle .fa-moon');
-const sunIcon = document.querySelector('#theme-toggle .fa-sun');
+const themeIcon = themeToggle.querySelector('.toggle-icon');
+const moonIcon = themeToggle.querySelector('.fa-moon');
+const sunIcon = themeToggle.querySelector('.fa-sun');
+const nava = document.getElementById('nava');
+const hamburger = document.getElementById('menu-icon');
+
+// Function to toggle dark mode classes
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+  nava.classList.toggle('dark-mode');
+  nava.classList.toggle('white-links');
+
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  moonIcon.style.opacity = isDarkMode ? '1' : '0';
+  sunIcon.style.opacity = isDarkMode ? '0' : '1';
+  themeIcon.style.transform = isDarkMode ? 'translateX(26px)' : 'translateX(0)';
+
+  // Store the user's preference in localStorage
+  localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
+}
 
 themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-  if (document.body.classList.contains('dark-mode')) {
-    moonIcon.style.opacity = '1';
-    sunIcon.style.opacity = '0';
-    themeIcon.style.transform = 'translateX(26px)';
-  } else {
-    moonIcon.style.opacity = '0';
-    sunIcon.style.opacity = '1';
-    themeIcon.style.transform = 'translateX(0)';
-  }
+  toggleDarkMode();
 });
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -32,16 +41,27 @@ const sr = ScrollReveal({
   distance: '100px',
   duration: 2000,
   reset: true
-})
+});
 sr.reveal('.home-text', {
   delay: 120,
   origin: 'top'
-})
+});
 sr.reveal('.home-img', {
   delay: 180,
   origin: 'top'
-})
+});
 sr.reveal('.about, .services, .cta, .audio-sec, .resume, .contact, .copyright', {
   delay: 240,
   origin: 'bottom'
-})
+});
+
+// Check if the user has a preference for dark mode
+const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// Check if the user previously set the mode in localStorage
+const storedDarkMode = localStorage.getItem('darkMode');
+
+// Set dark mode by default if the user has a preference for it or previously set the mode
+if (prefersDarkMode || storedDarkMode === 'true') {
+  toggleDarkMode();
+}
