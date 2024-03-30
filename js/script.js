292761393caa1625786f2b909bc9dd804ca1dd41
Rohan Biswas <rohan.biswas2020@uem.edu.in>
2024-03-30 23:36:42 +0530

    Freelancing

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ sr.reveal('.home-img', {
   delay: 180,
   origin: 'top'
 });
-sr.reveal('.about, .services, .cta, .audio-sec, .resume, .contact, .copyright, .skills', {
+sr.reveal('.about, .services, .cta, .audio-sec, .resume, .contact, .copyright, .works, .skills', {
   delay: 240,
   origin: 'bottom'
 });
